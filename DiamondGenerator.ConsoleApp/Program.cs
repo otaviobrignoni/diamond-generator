397e716d22f4d40db4024485464e88578096a8db
Otávio Rodrigo Pereira Brignoni <otavio.r01@aluno.ifsc.edu.br>
2025-03-25 17:25:30 -0300

    Added method to get and validate an user entry

--- DiamondGenerator.ConsoleApp/Program.cs
+++ DiamondGenerator.ConsoleApp/Program.cs
@@ -4,6 +4,9 @@ internal class Program
 {
     static void Main(string[] args)
     {
-
+        Console.WriteLine("Diamond Generator");
+        int userNumber = Utils.GetValidUserNumber();
+        Console.WriteLine($"The informed number is {userNumber}");
+        Console.ReadKey();
     }
 }
---
+++ DiamondGenerator.ConsoleApp/Utils.cs
@@ -0,0 +1,16 @@
+﻿namespace DiamondGenerator.ConsoleApp
+{
+    class Utils
+    {
+        public static int GetValidUserNumber()
+        {
+            int number;
+            Console.Write("Enter an odd number -> ");
+            while (!int.TryParse(Console.ReadLine(), out number) || number % 2 == 0 || number <= 0)
+            {
+                Console.Write("Invalid entry, try again -> ");
+            }
+            return number;
+        }
+    }
+}
